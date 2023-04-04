09065d163b8dd26fba21d90d226183cb43c3e856
Sani Rogers <sanirogers138@gmail.com>
2023-04-04 14:38:47 -0400

    work on sidebar

--- client/src/components/BottomNavigationBar.jsx
+++ client/src/components/BottomNavigationBar.jsx
@@ -1,6 +1,8 @@
-import { useState } from "react"
-import styles from "./BottomNavigationBar.module.scss"
-import { Drawer } from "@mui/material"
+import { useState } from "react";
+import styles from "./BottomNavigationBar.module.scss";
+import { Drawer, ListItemIcon, ListItemText } from "@mui/material";
+import { Divider, List, ListItem, ListItemButton, Typography } from "@mui/joy";
+import { GiFruitBowl } from "react-icons/gi";
 
 const BottomNavigationBar = () => {
     const [sidebarState, setSidebarState] = useState(false)
@@ -15,8 +17,43 @@ const BottomNavigationBar = () => {
             <a href="#">Edible Seeds</a>
             <a href="#">Deals</a>
             <a href="#">Vendors</a>
-            <Drawer anchor="left" open={sidebarState} onClose={() => setSidebarState(false)}>
-                <h3>BLOOD</h3>
+            <Drawer sx={{ display: "flex", justifyContent: "center"}} anchor="left" open={sidebarState} onClose={() => setSidebarState(false)}>
+                <div className={styles.sidebarContainer}>
+                    <div className={styles.sidebarHeaderContainer}>
+                        <span>Hello, Me</span>
+                    </div>
+                    <ul>
+                        <li><div>Fruit & Veggies</div></li>
+                        <li><a href="#">Apples</a></li>
+                        <li><a href="#">Berries</a></li>
+                        <li><a href="#">Grapes</a></li>
+                        <li><a href="#">Bananas</a></li>
+                        <li><a href="#">Pumpkins</a></li>
+                        <li><a href="#">Beets</a></li>
+                        <li><a href="#">Carrots</a></li>
+                        <li><a href="#">Exotic Selections</a></li>
+                        <li><div>Dairy Products</div></li>
+                        <li><a href="#">Cow Milk</a></li>
+                        <li><a href="#">Goat Milk</a></li>
+                        <li><a href="#">Camel Milk</a></li>
+                        <li><a href="#">Cheeses</a></li>
+                    </ul>
+                    <div>
+                        <h1>Dairy Products</h1>
+                    </div>
+                    <div>
+                        <h1>Flowers</h1>
+                    </div>
+                    <div>
+                        <h1>Eggs</h1>
+                    </div>
+                    <div>
+                        <h1>Edible Seeds</h1>
+                    </div>
+                    <div>
+                        <h1>My Account</h1>
+                    </div>
+                </div>
             </Drawer>
         </div>
     )
