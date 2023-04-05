32cd7e646768306acaf266a1b4a6e433aa7596d9
Sani Rogers <sanirogers138@gmail.com>
2023-04-05 15:06:28 -0400

    more work on FilterAccordion. Some styling still needs to be done

--- client/src/pages/ProductsPage.jsx
+++ client/src/pages/ProductsPage.jsx
@@ -1,4 +1,4 @@
-import { Radio } from "@mui/joy";
+import { Radio, RadioGroup } from "@mui/joy";
 import FilterAccordion from "../components/FilterAccordion";
 import FilterSidebar from "../components/FilterSidebar";
 import ProductContainer from "../components/ProductContainer";
@@ -10,94 +10,13 @@ const ProductsPage = () => {
         <div className={styles.productsPageWrapper}>
             <FilterSidebar>
                 <FilterAccordion>
-                    <Radio checked={false}
-                    value="a"
-                    name="type-filter"
-                    label="a"/>
-                    <Radio checked={false}
-                    value="b"
-                    name="type-filter"
-                    label="b"/>
-                    <Radio checked={false}
-                    value="c"
-                    name="type-filter"
-                    label="c"/>
-                    <Radio checked={false}
-                    value="d"
-                    name="type-filter"
-                    label="d"/>
-                </FilterAccordion>
-                <FilterAccordion>
-                    <Radio checked={false}
-                    value="a"
-                    name="type-filter"
-                    label="a"/>
-                    <Radio checked={false}
-                    value="b"
-                    name="type-filter"
-                    label="b"/>
-                    <Radio checked={false}
-                    value="c"
-                    name="type-filter"
-                    label="c"/>
-                    <Radio checked={false}
-                    value="d"
-                    name="type-filter"
-                    label="d"/>
-                </FilterAccordion>
-                <FilterAccordion>
-                    <Radio checked={false}
-                    value="a"
-                    name="type-filter"
-                    label="a"/>
-                    <Radio checked={false}
-                    value="b"
-                    name="type-filter"
-                    label="b"/>
-                    <Radio checked={false}
-                    value="c"
-                    name="type-filter"
-                    label="c"/>
-                    <Radio checked={false}
-                    value="d"
-                    name="type-filter"
-                    label="d"/>
-                </FilterAccordion>
-                <FilterAccordion>
-                    <Radio checked={false}
-                    value="a"
-                    name="type-filter"
-                    label="a"/>
-                    <Radio checked={false}
-                    value="b"
-                    name="type-filter"
-                    label="b"/>
-                    <Radio checked={false}
-                    value="c"
-                    name="type-filter"
-                    label="c"/>
-                    <Radio checked={false}
-                    value="d"
-                    name="type-filter"
-                    label="d"/>
-                </FilterAccordion>
-                <FilterAccordion>
-                    <Radio checked={false}
-                    value="a"
-                    name="type-filter"
-                    label="a"/>
-                    <Radio checked={false}
-                    value="b"
-                    name="type-filter"
-                    label="b"/>
-                    <Radio checked={false}
-                    value="c"
-                    name="type-filter"
-                    label="c"/>
-                    <Radio checked={false}
-                    value="d"
-                    name="type-filter"
-                    label="d"/>
+                    <RadioGroup defaultValue="allproducts" >
+                        <Radio size="lg" value="allproducts" label="All Products" variant="soft" color="success"/>
+                        <Radio size="lg" value="fruit" label="Fruit" variant="soft" color="success"/>
+                        <Radio size="lg" value="vegetables" label="Vegetables" variant="soft"  color="success"/>
+                        <Radio size="lg" value="eggs" label="Eggs" variant="soft" color="success"/>
+                        <Radio size="lg" value="milk" label="Milk" variant="soft" color="success"/>
+                    </RadioGroup>
                 </FilterAccordion>
             </FilterSidebar>
             <ProductContainer>
