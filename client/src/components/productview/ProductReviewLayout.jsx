b06958d7df2f1d86dfb46dcce9062d92fcaa6ca4
Sani Rogers <sanirogers138@gmail.com>
2023-04-17 19:57:07 -0400

    logged in change site view

--- client/src/components/productview/ProductReviewLayout.jsx
+++ client/src/components/productview/ProductReviewLayout.jsx
@@ -1,17 +1,23 @@
+import { useSelector } from "react-redux";
 import styles from "./ProductReviewLayout.module.scss";
 import { FaPlus } from "react-icons/fa"
+import { selectLoggedIn } from "../../reducers/loginSlice";
 
 
 const ProductReviewLayout = ({ children }) => {
+
+    const loggedInState = useSelector(selectLoggedIn)
 
     return (
         <div className={styles.productReviewParentContainer}>
             <h2>Customer Reviews</h2>
             <div className={styles.productReviewContainer}>
-                <div className={styles.addReviewCard}>
+                {loggedInState ? (
+                    <div className={styles.addReviewCard}>
                     <h3>Add your own!</h3>
                     <FaPlus size={260} color="2C6E49"/>
                 </div>
+                ) : null}
                 {children}
             </div>
         </div>
